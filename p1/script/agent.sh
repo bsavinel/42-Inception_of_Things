12ce7e6580cd001a52093685e785b5696102fff1
bsavinel <bsavinel@student.42.fr>
2024-01-07 15:08:03 +0100

    WIP: add script to install kubecl et k3s

---
+++ p1/script/agent.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo A
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://k3s.example.com --token mypassword" sh -s -
---
+++ p1/script/server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo C
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --flannel-backend none --token 12345
