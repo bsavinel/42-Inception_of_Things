59a2b0dbc56d9e90fb23ca7c217715093e2bf66b
bsavinel <bsavinel@student.42.fr>
2024-01-17 07:24:57 -0500

    WIP: starting ArgoCD

--- p3/scripts/setup/setup-server.sh
+++ p3/scripts/setup/setup-server.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
-echo ok
-su -u root -p vagrant
+# echo ok
+# # su -u root -p vagrant
 
-apt-get install -y curl
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+# # sudo apt-get install -y curl
+# # curl -fsSL https://get.docker.com -o get-docker.sh
+# # sh get-docker.sh
 
-usermod -aG docker $USER
-docker info
+# # sudo usermod -aG docker $USER
+# # sudo chmod 666 /var/run/docker.sock
+# # docker info
 
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# k3d version
+
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# sudo chmod 777 /usr/local/bin/ 
+# install kubectl /usr/local/bin/kubectl
+# kubectl version --client
+
+
+
+# k3d cluster create --config /vagrant/scripts/k3d/config.yml
+
+
+sudo wget https://github.com/rancher/k3d/releases/download/v3.0.0/k3d-linux-amd64 -O /usr/local/bin/k3d
+sudo chmod +x /usr/local/bin/k3d
 k3d version
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install kubectl /usr/local/bin/kubectl
-kubectl version --client
 
-
-
-k3d cluster create --config config.yml
+wget https://github.com/argoproj/argo-cd/raw/v1.6.2/manifests/install.yaml
