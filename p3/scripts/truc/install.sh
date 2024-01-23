bc0ea0a0660974bdcda1776444d0950dab937ad9
rpoder <rpoder@student.42.fr>
2024-01-23 09:14:51 -0500

    ADD: test setups files

---
+++ p3/scripts/truc/install.sh
@@ -0,0 +1,23 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ${USER}
+# su - ${USER}
+
+# kubectl install
+sudo apt-get update
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y kubectl
+
+# k3d install
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# argocd install
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+sudo chmod 777 /var/run/docker.sock
