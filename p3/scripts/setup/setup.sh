de9453d05e2de0b6c44d95de1a55c282555a174c
rpoder <rpoder@student.42.fr>
2024-01-24 11:52:52 -0500

    ADD: kubectl wait all pods to be running

--- p3/scripts/setup/setup.sh
+++ p3/scripts/setup/setup.sh
@@ -8,7 +8,7 @@ kubectl create namespace dev
 # apply base config to cluster
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-sleep 60
+kubectl wait --for=condition=Ready pod --all -n "argocd"
 
 # forward ports between cluster and host
 kubectl port-forward svc/argocd-server -n argocd 8080:443 >/dev/null &
@@ -17,5 +17,9 @@ kubectl port-forward svc/argocd-server -n argocd 8080:443 >/dev/null &
 echo "Argo Cd admin password : "
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
+# create new argo cd app
 kubectl apply -f ../project.yml -n argocd
+
+# expose port to host
 kubectl port-forward svc/wil42-playground -n dev 8888:7171 >/dev/null &
+
